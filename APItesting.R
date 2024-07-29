51e3e7a794aa1e7d336c9a42383ee3fc12a0fe06
manuelgirbal <manuelgirbal@hotmail.com>
2024-07-28 21:39:09 -0300

    .

--- APItesting.R
+++ APItesting.R
@@ -2,25 +2,28 @@ library(spotifyr)
 library(tidyverse)
 library(lubridate)
 
+#### Configuration ####
+
 source("credentials.R")
 
+# Set Spotify Client ID and Secret
 Sys.setenv(SPOTIFY_CLIENT_ID = client_id)
 Sys.setenv(SPOTIFY_CLIENT_SECRET = client_secret)
-scope <- "user-read-recently-played"
 
-get_spotify_authorization_code(client_id,
-                               client_secret,
-                               scope)
+# Specify the required scope for the operations
+scope <- "user-read-recently-played playlist-read-private playlist-read-collaborative"
 
+# Obtain the authorization code with the specified scope
+access_token <- get_spotify_authorization_code(scope = scope)
+
+
+#### Testing analysis ####
 
 # Get your most recently played tracks
-
-get_my_recently_played(limit = 20)
-
-library(kableExtra)
-get_my_recently_played(limit = 5) %>% 
+get_my_recently_played(limit = 5, authorization = access_token) |> 
   mutate(artist.name = map_chr(track.artists, function(x) x$name[1]),
-         played_at = as_datetime(played_at)) %>% 
-  select(track.name, artist.name, track.album.name, played_at) %>% 
-  kable()
+         played_at = as_datetime(played_at)) |> 
+  select(track.name, artist.name, track.album.name, played_at)
 
+# Playlists analysis
+get_my_playlists(limit = 5,authorization = access_token)
