4231be24772f422f569eac83b58520fc8166eaad
manuelgirbal <manuelgirbal@protonmail.com>
2024-08-02 15:29:07 -0300

    .

--- APItesting.R
+++ APItesting.R
@@ -19,11 +19,52 @@ access_token <- get_spotify_authorization_code(scope = scope)
 
 #### Testing analysis ####
 
-# Get your most recently played tracks
-get_my_recently_played(limit = 5, authorization = access_token) |> 
-  mutate(artist.name = map_chr(track.artists, function(x) x$name[1]),
-         played_at = as_datetime(played_at)) |> 
-  select(track.name, artist.name, track.album.name, played_at)
+## Playlists analysis
+i <- 0
+rows <- 0
+my_playlists <- tibble()
 
-# Playlists analysis
-get_my_playlists(limit = 5,authorization = access_token)
+while (i <= rows) {
+  
+  playlists <- get_my_playlists(authorization = access_token,
+                                   limit = 50,
+                                   offset = i)
+  
+  i <<- i+50
+  my_playlists <<- bind_rows(my_playlists, playlists)
+  rows <<- nrow(my_playlists)
+  }
+
+
+# Temporary fix to get only owned playlists
+most_freq_user <- my_playlists |> 
+  count(owner.display_name) |> 
+  arrange(desc(n)) |> 
+  slice(1) |> 
+  transmute(user = owner.display_name)
+
+my_playlists <- my_playlists |> 
+  filter(owner.display_name == most_freq_user[[1]]) |> 
+  select(id, name)
+
+
+# Getting tracks from two playlists to compare
+
+ruta <- my_playlists |> 
+  filter(name == 'Ruta Provincial') |> 
+  select(1)
+
+ruta_tracks <- get_playlist_tracks(ruta) 
+
+pampeano <- my_playlists |> 
+  filter(name == 'Pampeano - Stoner') |> 
+  select(1)
+
+pampeano_tracks <- get_playlist_tracks(pampeano) 
+
+
+# Getting features both from playlists and songs:
+
+### SEGUIR CON: 
+# https://www.rdocumentation.org/packages/spotifyr/versions/2.1.1/topics/get_playlist_audio_features
+# https://www.rdocumentation.org/packages/spotifyr/versions/2.1.1/topics/get_track_audio_features
